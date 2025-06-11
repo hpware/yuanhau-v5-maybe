b12cd2128772022869e8d6fb9c83d0ac7e827035
吳元皓 <hw@yuanhau.com>
2025-06-11 10:07:20 +0800

    Fix some stuff.

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,7 @@
-export async function GET(request: Request) {
+export async function GET() {
   return new Response("GET response");
 }
 
-export async function POST(request: Request) {
+export async function POST() {
   return new Response("POST response");
 }
