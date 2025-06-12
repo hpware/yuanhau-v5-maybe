3a885c47ed7b736587ab1a8182ddc02491ea4dea
吳元皓 <hw@yuanhau.com>
2025-06-12 13:07:43 +0800

    Add more 3D features.

--- src/app/3d/[slug]/page.tsx
+++ src/app/3d/[slug]/page.tsx
@@ -1,11 +1,26 @@
+import { unstable_ViewTransition as ViewTransition } from "react";
+import Link from "next/link";
+
 export default async function Page(props: {
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await props.params;
 
   return (
-    <div>
-      <h1>Here is your slug: {slug}</h1>
+    <div className="justify-center align-center text-center m-1 absolute inset-0 flex flex-col">
+      <ViewTransition name="title">
+        <h1 className="text-4xl font-bold text-center mb-4 dark:text-white">
+          3D Files
+        </h1>
+        <Link
+          href="/"
+          className="text-blue-gray-500 hover:underline text-center"
+        >
+          ← Back
+        </Link>
+      </ViewTransition>
+      <span>Here is your slug: {slug}</span>
+      Download File: <a href={"/3d/" + slug + "/download"}>Download</a>
     </div>
   );
 }
