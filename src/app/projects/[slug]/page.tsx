66d97b375cb19a6f65582356c9e754c4a4db7e66
吳元皓 <hw@yuanhau.com>
2025-06-15 18:00:40 +0800

    Should work.

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,8 +1,11 @@
-export default function Page(props: { params: Promise<{ slug: string }> }) {
+export default async function Page(props: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await props.params;
   return (
     <div className="absolute inset-0 m-2 flex flex-col justify-center items-center text-center align-middle">
       <h1>HI!</h1>
-      <h4>{params.slug}</h4>
+      <h4>{slug}</h4>
     </div>
   );
 }
