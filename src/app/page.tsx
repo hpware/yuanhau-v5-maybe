31076ff6a602dcc9c9f67df66b2846682674e136
吳元皓 <hw@yuanhau.com>
2025-06-19 07:16:53 +0800

    Add all socials.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,28 +13,6 @@ import {
 export const dynamic = "force-dynamic";
 export const revalidate = 3000;
 
-export default function Page() {
-  const sqlContent = "Hi";
-  return (
-    <Layout tab="/">
-      <div className="absolute inset-0 align-middle flex flex-col justify-center text-center">
-        <ViewTransition name="title">
-          <h1 className="text-4xl font-bold text-center mb-4 dark:text-white">
-            元皓的網站 v5
-          </h1>
-        </ViewTransition>
-        {socials.map((i) => (
-          <a href={i.url} aria-label={i.name} key={i.url}>
-            {i.icon}
-          </a>
-        ))}
-      </div>
-      <div className="h-screen"></div>
-      <div className="">{sqlContent}</div>
-    </Layout>
-  );
-}
-
 const socials = [
   {
     name: "github",
@@ -46,17 +24,10 @@ const socials = [
     url: "https://instagram.com/yhw_tw",
     icon: <InstagramIcon />,
   },
-];
-{
-  /**
-  <div className="flex flex-row justify-center align-middle text-center gap-2">
-    <a href="https://github.com/hpware" aria-label="github" id="github">
-      <GithubIcon />
-    </a>
-    <a href="https://instagram.com/yhw_tw" aria-label="instagram">
-      <InstagramIcon />
-    </a>
-    <a href="https://threads.net/yhw_tw" aria-label="threads">
+  {
+    name: "threads",
+    url: "https://threads.net/@yhw_tw",
+    icon: (
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -67,19 +38,45 @@ const socials = [
       >
         <path d="M6.321 6.016c-.27-.18-1.166-.802-1.166-.802.756-1.081 1.753-1.502 3.132-1.502.975 0 1.803.327 2.394.948s.928 1.509 1.005 2.644q.492.207.905.484c1.109.745 1.719 1.86 1.719 3.137 0 2.716-2.226 5.075-6.256 5.075C4.594 16 1 13.987 1 7.994 1 2.034 4.482 0 8.044 0 9.69 0 13.55.243 15 5.036l-1.36.353C12.516 1.974 10.163 1.43 8.006 1.43c-3.565 0-5.582 2.171-5.582 6.79 0 4.143 2.254 6.343 5.63 6.343 2.777 0 4.847-1.443 4.847-3.556 0-1.438-1.208-2.127-1.27-2.127-.236 1.234-.868 3.31-3.644 3.31-1.618 0-3.013-1.118-3.013-2.582 0-2.09 1.984-2.847 3.55-2.847.586 0 1.294.04 1.663.114 0-.637-.54-1.728-1.9-1.728-1.25 0-1.566.405-1.967.868ZM8.716 8.19c-2.04 0-2.304.87-2.304 1.416 0 .878 1.043 1.168 1.6 1.168 1.02 0 2.067-.282 2.232-2.423a6.2 6.2 0 0 0-1.528-.161" />
       </svg>
-    </a>
-    <a href="https://twitter.com/ictechz" aria-label="Twitter (X)">
-      <TwitterIcon />
-    </a>
-    <a href="https://youtube.com/@號" aria-label="youtube" id="youtube">
-      <YoutubeIcon />
-    </a>
-    <a
-      href="mailto:public+yhcom-v5@yuanhau.com"
-      aria-label="email"
-      id="email"
-    >
-      <MailIcon />
-    </a>
-  </div> */
+    ),
+  },
+  {
+    name: "Twitter (X)",
+    url: "https://twitter.com/ictechz",
+    icon: <TwitterIcon />,
+  },
+  {
+    name: "youtube",
+    url: "https://youtube.com/@號",
+    icon: <YoutubeIcon />,
+  },
+  {
+    name: "email",
+    url: "mailto:public+yhcom-v5@yuanhau.com",
+    icon: <MailIcon />,
+  },
+];
+
+export default function Page() {
+  const sqlContent = "Hi";
+  return (
+    <Layout tab="/">
+      <div className="absolute inset-0 align-middle flex flex-col justify-center text-center">
+        <ViewTransition name="title">
+          <h1 className="text-4xl font-bold text-center mb-4 dark:text-white">
+            元皓的網站 v5
+          </h1>
+        </ViewTransition>
+        <div className="flex flex-row justify-center align-middle text-center gap-2">
+          {socials.map((i) => (
+            <a href={i.url} aria-label={i.name} key={i.url}>
+              {i.icon}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="h-screen"></div>
+      <div className="">{sqlContent}</div>
+    </Layout>
+  );
 }
