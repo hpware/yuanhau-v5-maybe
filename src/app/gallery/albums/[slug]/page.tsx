71d850a0080ace81375c6d58a6fa44c941718856
吳元皓 <hw@yuanhau.com>
2025-07-03 21:27:32 +0800

    Fix some stuff.

--- src/app/gallery/albums/[slug]/page.tsx
+++ src/app/gallery/albums/[slug]/page.tsx
@@ -1,5 +1,5 @@
 //import sql from "@/components/pg";
-import { notFound } from "next/navigation";
+//import { notFound } from "next/navigation";
 export const dynamic = "force-dynamic";
 export const revalidate = 3600;
 
@@ -7,9 +7,6 @@ export default async function Page(props: {
   params: Promise<{ slug: string }>;
 }) {
   const { slug } = await props.params;
-  if (1 === 0) {
-    notFound();
-  }
   return (
     <div>
       <span>Hi {slug}</span>
---
+++ src/app/gallery/image/[slug]/page.tsx
@@ -0,0 +1,15 @@
+//import sql from "@/components/pg";
+//import { notFound } from "next/navigation";
+export const dynamic = "force-dynamic";
+export const revalidate = 3600;
+
+export default async function Page(props: {
+  params: Promise<{ slug: string }>;
+}) {
+  const { slug } = await props.params;
+  return (
+    <div>
+      <span>Hi {slug}</span>
+    </div>
+  );
+}
